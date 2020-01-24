d31eb839cf87128b6f80ceb8a40d27c2fb413d7d
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2020-01-24 16:28:39 +0700

    rekt

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main(){	
 
+	int year=1;
+	float initialloan=0.00,interestrateperyear=0.00,Enteramountyoucanpayperyear=0.00,imainterest=0.00,atarashiippy=0.00;
+	cout << "Enter initial loan : ";
+	cin >> initialloan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> 	interestrateperyear;
+	cout << "Enter amount you can pay per year : ";
+	cin >> Enteramountyoucanpayperyear;
+	
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -17,14 +26,25 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	for (;;){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << initialloan;
+	imainterest = (initialloan*interestrateperyear)/100;
+	cout << setw(13) << left << imainterest;
+	initialloan = imainterest+initialloan;
+	cout << setw(13) << left << initialloan;
+	if(Enteramountyoucanpayperyear>initialloan){
+		 Enteramountyoucanpayperyear=initialloan;
+	cout << setw(13) << left <<  Enteramountyoucanpayperyear;
+	}else{ cout << setw(13) << left << Enteramountyoucanpayperyear;}
+		initialloan=initialloan-Enteramountyoucanpayperyear;
+
+	cout << setw(13) << left << initialloan;
 	cout << "\n";	
+	year++;
+	if(initialloan < 1)break;
+	}
 	
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N,int M){
+	if (N<=0 || M<=0)cout << "Invalid Input";
+	else for(int i=N;i>0;i--){
+		for(int j=M;j>0;j--){
+			cout << "O";
+		}
+		cout << "\n";
+	}
+}
 
 int main(){
 	
